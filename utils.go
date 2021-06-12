04c4707f8d16cb648e404f14e2c3d321b820495d
A. Mogilevskiy <dark.qvark@gmail.com>
2021-06-12 15:06:59 +0300

    feat (core) BindAndValidate init

--- utils.go
+++ utils.go
@@ -1,8 +1,11 @@
 package gofnd
 
 import (
+	"fmt"
 	"mime"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func ParseContentType(header http.Header) string {
@@ -13,4 +16,16 @@ func ParseContentType(header http.Header) string {
 	contentType, _, _ := mime.ParseMediaType(contentTypeRaw[0])
 
 	return contentType
+}
+
+func BindAndValidate(ctx echo.Context, body interface{}) (err error) {
+	if err = ctx.Bind(body); err != nil {
+		return fmt.Errorf("gofnd[.BindAndValidate][1]: %+v", err)
+	}
+
+	if err = ctx.Validate(body); err != nil {
+		return fmt.Errorf("gofnd[.BindAndValidate][2]: %+v", err)
+	}
+
+	return
 }
