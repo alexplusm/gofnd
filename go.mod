3a69e257f115fe871cc97193a97f20463b8a5e5a
A. Mogilevskiy <dark.qvark@gmail.com>
2021-06-12 13:27:06 +0300

    feat (core) init LoggerMiddleware

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/alexplusm/gofnd
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.3.0
+require (
+	github.com/labstack/echo/v4 v4.3.0
+	github.com/sirupsen/logrus v1.8.1
+)
--- middlewares.go
+++ middlewares.go
@@ -2,8 +2,11 @@ package gofnd
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	log "github.com/sirupsen/logrus"
 )
 
 func ContentTypeMiddleware(expectedContentType string) echo.MiddlewareFunc {
@@ -18,4 +21,42 @@ func ContentTypeMiddleware(expectedContentType string) echo.MiddlewareFunc {
 			return echo.NewHTTPError(http.StatusUnsupportedMediaType, "Unsupported Media Type")
 		}
 	}
+}
+
+func LoggerMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		return logrusMiddlewareHandler(ctx, next)
+	}
+}
+
+func logrusMiddlewareHandler(c echo.Context, next echo.HandlerFunc) error {
+	req := c.Request()
+	res := c.Response()
+	start := time.Now()
+	if err := next(c); err != nil {
+		c.Error(err)
+	}
+	stop := time.Now()
+
+	p := req.URL.Path
+
+	bytesIn := req.Header.Get(echo.HeaderContentLength)
+
+	log.WithFields(map[string]interface{}{
+		"time_rfc3339":  time.Now().Format(time.RFC3339),
+		"remote_ip":     c.RealIP(),
+		"host":          req.Host,
+		"uri":           req.RequestURI,
+		"method":        req.Method,
+		"path":          p,
+		"referer":       req.Referer(),
+		"user_agent":    req.UserAgent(),
+		"status":        res.Status,
+		"latency":       strconv.FormatInt(stop.Sub(start).Nanoseconds()/1000, 10),
+		"latency_human": stop.Sub(start).String(),
+		"bytes_in":      bytesIn,
+		"bytes_out":     strconv.FormatInt(res.Size, 10),
+	}).Info("Handled request")
+
+	return nil
 }
