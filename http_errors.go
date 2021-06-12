3a2ada18d058638c13a492c26565873d5d561aa8
A. Mogilevskiy <dark.qvark@gmail.com>
2021-06-12 13:12:48 +0300

    feat (core) http errors

---
+++ http_errors.go
@@ -0,0 +1,30 @@
+package gofnd
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func NewNotFoundHTTPError(msg string) *echo.HTTPError {
+	return echo.NewHTTPError(http.StatusNotFound, msg)
+}
+
+// NewBadRequestHTTPError : if arg == "" -> use default message "bad request"
+func NewBadRequestHTTPError(msg string, err error) *echo.HTTPError {
+	m := "bad request"
+
+	if msg != "" {
+		m += ": " + msg
+	}
+
+	return echo.NewHTTPError(http.StatusBadRequest, m).SetInternal(err)
+}
+
+func NewInternalServerError(err error) *echo.HTTPError {
+	return echo.ErrInternalServerError.SetInternal(err)
+}
+
+func AlreadyExistError(msg string, err error) *echo.HTTPError {
+	return echo.NewHTTPError(http.StatusBadRequest, msg).SetInternal(err)
+}
