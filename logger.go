957fb3b9c3407c895b4bc3cfa6d43eb82258ce4f
A. Mogilevskiy <dark.qvark@gmail.com>
2021-07-03 13:05:43 +0300

    feat (core) add logging

---
+++ logger.go
@@ -0,0 +1,45 @@
+package gofnd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	logsDir = "logs"
+)
+
+// TODO: upd logging: source: https://www.honeybadger.io/blog/golang-logging/
+
+func SetupLogger(fileName string, writeToFile bool) {
+	log.SetFormatter(&log.JSONFormatter{})
+
+	logLevel := log.TraceLevel
+
+	logLevelRaw := "TRACE"
+
+	switch logLevelRaw {
+	case "INFO":
+		logLevel = log.InfoLevel
+	case "DEBUG":
+		logLevel = log.DebugLevel
+	case "TRACE":
+		logLevel = log.TraceLevel
+	}
+
+	log.SetLevel(logLevel)
+
+	if writeToFile {
+		logFilePath := filepath.Join(logsDir, fmt.Sprintf("%v.log", fileName))
+		file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// TODO: need close file???
+		log.SetOutput(file)
+	}
+}
